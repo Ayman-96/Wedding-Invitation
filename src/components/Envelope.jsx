66a95197093b965c99047f93950d11cfe75e824b
ayman <aemanharde@gmail.com>
2026-08-23 19:21:27 +0300

    User Guider

--- src/components/Envelope.jsx
+++ src/components/Envelope.jsx
@@ -4,7 +4,8 @@ import frontBodyImg from "../assets/front-envelope.png";
 import sealImg from "../assets/wax.png";
 import flapOpenedImg from "../assets/flap-opened.png";
 import bg from "../assets/envelope-bg.png";
-import { useRef, useState } from "react";
+import arrow from "../assets/arrow-bg.png";
+import { useEffect, useRef, useState } from "react";
 import Card from "./Card";
 
 function Envelope() {
@@ -40,9 +41,12 @@ function Envelope() {
       className={`flex items-center justify-center w-full h-screen overflow-x-hidden bg-cover bg-no-repeat bg-center ${cardRised && "animate-fade-out"}`}
       onPointerDown={handlePointerDown}
       onPointerUp={handlePointerUp}
-      style={{ perspective: "1000px", backgroundImage: `url(${bg})` }}
+      style={{
+        perspective: "1000px",
+        backgroundImage: `url(${bg})`,
+      }}
     >
-      <div className="relative w-[200vw] max-w-[800px] aspect-[4/3]">
+      <div className="relative w-[200vw] max-w-[800px] aspect-[4/3] -ml-2">
         <img
           src={backBodyImg}
           draggable="false"
@@ -71,6 +75,74 @@ function Envelope() {
           className={`select-none absolute inset-0 -top-45 left-1 w-[485px] max-w-2xl opacity-0 origin-top  ${flapOpened ? "animate-flap-close" : ""}`}
         />
       </div>
+      <Guider step={step} />
+    </div>
+  );
+}
+function Guider({ step }) {
+  const [fading, setFading] = useState(false);
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setFading(false);
+    const timer = setTimeout(() => setFading(true), 3000);
+    return () => clearTimeout(timer);
+  }, [step]);
+  return (
+    <div className="absolute z-100">
+      {step === 0 ? (
+        <svg
+          width="140"
+          height="62"
+          viewBox="0 0 64 32"
+          className={`mt-100 ${fading ? "animate-fade-out-arrow" : "animate-sway"}`}
+        >
+          <defs>
+            <pattern
+              id="arrowImg"
+              patternUnits="userSpaceOnUse"
+              width="84"
+              height="52"
+            >
+              <image
+                href={arrow}
+                x="0"
+                y="0"
+                width="84"
+                height="32"
+                preserveAspectRatio="xMidYMid slice"
+              />
+            </pattern>
+          </defs>
+          <path d="M0 8 H36 V0 L64 16 L36 32 V24 H0 Z" fill="url(#arrowImg)" />
+        </svg>
+      ) : (
+        <svg
+          width="140"
+          height="64"
+          viewBox="0 0 64 32"
+          className={`-rotate-90 mt-120 ${fading ? "animate-fade-out-arrow" : "animate-sway"}`}
+        >
+          <defs>
+            <pattern
+              id="arrowImg"
+              patternUnits="userSpaceOnUse"
+              width="64"
+              height="32"
+            >
+              <image
+                href={arrow}
+                x="0"
+                y="0"
+                width="64"
+                height="32"
+                preserveAspectRatio="xMidYMid slice"
+              />
+            </pattern>
+          </defs>
+          <path d="M0 8 H36 V0 L64 16 L36 32 V24 H0 Z" fill="url(#arrowImg)" />
+        </svg>
+      )}
     </div>
   );
 }
